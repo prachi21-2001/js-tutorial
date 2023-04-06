19e7b2a15d048e11f84452c77e470542b456c15d
prachi21-2001 <prachisin26@gmail.com>
2023-04-06 17:48:48 +0530

    array

--- js/first.js
+++ js/first.js
@@ -1,4 +1,15 @@
-alert("hello world");
-document.getElementById("h").innerHTML="prachi";
-document.write("sinha");
-console.log("hello");
+// alert("hello world");
+// document.getElementById("h").innerHTML="prachi";
+// document.write("sinha");
+// console.log("hello");
+// let str='prachi sinha';
+// console.log(str.length);
+// array
+// let book1="math";
+// let book2="physics";
+// let book3="bio";
+// let book4="english";
+// console.log(book3);
+let book=["math","bio","physics","cse","egf"];//multiple values in one variable
+book[1]="english"; //
+console.log(book);
